b9856de15a81be13a4e49d28e6d44d76c4794d3a
Daniel Yang <yangafc@bc.edu>
2019-08-26 22:09:33 -0400

    Initial Commit

---
+++ sample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  sample
+//
+//  Created by Daniel Yang on 8/26/19.
+//  Copyright © 2019 Daniel Yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
